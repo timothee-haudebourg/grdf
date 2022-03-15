84d2620c791d3b0ad9a200012b3006f8d3a6a5d9
Timothée Haudebourg <timothee.haudebourg@hemisemi.fr>
2022-03-15 13:36:13 +0100

    More `loc` related functions.

--- src/loc.rs
+++ src/loc.rs
@@ -1,6 +1,6 @@
 //! Compatibility layer with the [`locspan`](https://crates.io/crates/locspan) library.
 //!
-//! Provides dataset implementations that also store the source location of each quad.
+//! Provides dataset implementations that also saves the source location of each quad.
 use locspan::{Loc, Location};
 use rdf_types::loc::LocQuad;
 use std::hash::Hash;
@@ -22,14 +22,9 @@ pub struct Object<O, F> {
 	pub locations: Loc<Locations<F>, F>,
 }
 
-/// `HashDataset` with location information.
-pub type HashDataset<S, P, O, G, F> = crate::HashDataset<S, P, Object<O, F>, G>;
-
-impl<S: Eq + Hash, P: Eq + Hash, O: Eq + Hash, G: Eq + Hash, F: Eq + Hash>
-	HashDataset<S, P, O, G, F>
-{
-	pub fn insert_loc(
-		&mut self,
+impl<O, F> Object<O, F> {
+	/// Creates a new quad where all location information is stored in the `Object<O, F>`.
+	pub fn new<S, P, G>(
 		Loc(rdf_types::Quad(Loc(s, s_loc), Loc(p, p_loc), Loc(o, o_loc), g), loc): LocQuad<
 			S,
 			P,
@@ -37,13 +32,13 @@ impl<S: Eq + Hash, P: Eq + Hash, O: Eq + Hash, G: Eq + Hash, F: Eq + Hash>
 			G,
 			F,
 		>,
-	) {
+	) -> rdf_types::Quad<S, P, Self, G> {
 		let (locations, g) = match g {
 			Some(Loc(g, g_loc)) => (Locations::new(s_loc, p_loc, o_loc, Some(g_loc)), Some(g)),
 			None => (Locations::new(s_loc, p_loc, o_loc, None), None),
 		};
 
-		self.insert(rdf_types::Quad(
+		rdf_types::Quad(
 			s,
 			p,
 			Object {
@@ -51,7 +46,66 @@ impl<S: Eq + Hash, P: Eq + Hash, O: Eq + Hash, G: Eq + Hash, F: Eq + Hash>
 				locations: Loc(locations, loc),
 			},
 			g,
-		))
+		)
+	}
+
+	pub fn as_loc_quad<'s, 'p, 'g, S, P, G>(
+		&self,
+		s: &'s S,
+		p: &'p P,
+		g: Option<&'g G>,
+	) -> Loc<rdf_types::Quad<Loc<&'s S, &F>, Loc<&'p P, &F>, Loc<&O, &F>, Loc<&'g G, &F>>, &F> {
+		Loc(
+			rdf_types::Quad(
+				Loc(s, self.locations.subject().borrow()),
+				Loc(p, self.locations.predicate().borrow()),
+				Loc(&self.value, self.locations.object().borrow()),
+				g.map(|g| Loc(g, self.locations.graph().unwrap().borrow())),
+			),
+			self.locations.location().borrow(),
+		)
+	}
+
+	pub fn into_loc_quad<S, P, G>(self, s: S, p: P, g: Option<G>) -> LocQuad<S, P, O, G, F> {
+		let Loc(locations, loc) = self.locations;
+		let g_loc = locations.3;
+
+		Loc(
+			rdf_types::Quad(
+				Loc(s, locations.0),
+				Loc(p, locations.1),
+				Loc(self.value, locations.2),
+				g.map(|g| Loc(g, g_loc.unwrap())),
+			),
+			loc,
+		)
+	}
+}
+
+/// `HashDataset` with location information.
+pub type HashDataset<S, P, O, G, F> = crate::HashDataset<S, P, Object<O, F>, G>;
+
+impl<S: Eq + Hash, P: Eq + Hash, O: Eq + Hash, G: Eq + Hash, F: Eq + Hash>
+	HashDataset<S, P, O, G, F>
+{
+	/// Inserts a located quad into the dataset.
+	pub fn loc_insert(&mut self, quad: LocQuad<S, P, O, G, F>) {
+		self.insert(Object::new(quad))
+	}
+
+	/// Creates a borrowing iterator over the located quads of the dataset.
+	pub fn loc_quads(&self) -> LocQuads<Self> {
+		LocQuads(self.quads())
+	}
+
+	/// Turns this dataset into an iterator over the located quads of the dataset.
+	pub fn into_loc_quads(self) -> IntoLocQuads<Self>
+	where
+		S: Clone,
+		P: Clone,
+		G: Clone,
+	{
+		IntoLocQuads(self.into_quads())
 	}
 }
 
@@ -60,7 +114,7 @@ impl<S: Eq + Hash, P: Eq + Hash, O: Eq + Hash, G: Eq + Hash, F: Eq + Hash>
 {
 	fn extend<I: IntoIterator<Item = LocQuad<S, P, O, G, F>>>(&mut self, iter: I) {
 		for quad in iter {
-			self.insert_loc(quad)
+			self.loc_insert(quad)
 		}
 	}
 }
@@ -79,30 +133,24 @@ impl<S: Eq + Hash, P: Eq + Hash, O: Eq + Hash, G: Eq + Hash, F: Eq + Hash>
 pub type BTreeDataset<S, P, O, G, F> = crate::BTreeDataset<S, P, Object<O, F>, G>;
 
 impl<S: Ord, P: Ord, O: Ord, G: Ord, F: Ord> BTreeDataset<S, P, O, G, F> {
-	pub fn insert_loc(
-		&mut self,
-		Loc(rdf_types::Quad(Loc(s, s_loc), Loc(p, p_loc), Loc(o, o_loc), g), loc): LocQuad<
-			S,
-			P,
-			O,
-			G,
-			F,
-		>,
-	) {
-		let (locations, g) = match g {
-			Some(Loc(g, g_loc)) => (Locations::new(s_loc, p_loc, o_loc, Some(g_loc)), Some(g)),
-			None => (Locations::new(s_loc, p_loc, o_loc, None), None),
-		};
+	/// Inserts a located quad into the dataset.
+	pub fn loc_insert(&mut self, quad: LocQuad<S, P, O, G, F>) {
+		self.insert(Object::new(quad))
+	}
 
-		self.insert(rdf_types::Quad(
-			s,
-			p,
-			Object {
-				value: o,
-				locations: Loc(locations, loc),
-			},
-			g,
-		))
+	/// Creates a borrowing iterator over the located quads of the dataset.
+	pub fn loc_quads(&self) -> LocQuads<Self> {
+		LocQuads(self.quads())
+	}
+
+	/// Turns this dataset into an iterator over the located quads of the dataset.
+	pub fn into_loc_quads(self) -> IntoLocQuads<Self>
+	where
+		S: Clone,
+		P: Clone,
+		G: Clone,
+	{
+		IntoLocQuads(self.into_quads())
 	}
 }
 
@@ -111,7 +159,7 @@ impl<S: Ord, P: Ord, O: Ord, G: Ord, F: Ord> std::iter::Extend<LocQuad<S, P, O, 
 {
 	fn extend<I: IntoIterator<Item = LocQuad<S, P, O, G, F>>>(&mut self, iter: I) {
 		for quad in iter {
-			self.insert_loc(quad)
+			self.loc_insert(quad)
 		}
 	}
 }
@@ -123,5 +171,42 @@ impl<S: Ord, P: Ord, O: Ord, G: Ord, F: Ord> std::iter::FromIterator<LocQuad<S, 
 		let mut dataset = Self::new();
 		dataset.extend(iter);
 		dataset
+	}
+}
+
+pub struct LocQuads<'a, D: 'a + crate::Dataset>(D::Quads<'a>);
+
+impl<'a, O: 'a, F: 'a, D: 'a + crate::Dataset<Object = Object<O, F>>> Iterator for LocQuads<'a, D> {
+	type Item = Loc<
+		rdf_types::Quad<
+			Loc<&'a D::Subject, &'a F>,
+			Loc<&'a D::Predicate, &'a F>,
+			Loc<&'a O, &'a F>,
+			Loc<&'a D::GraphLabel, &'a F>,
+		>,
+		&'a F,
+	>;
+
+	fn next(&mut self) -> Option<Self::Item> {
+		self.0
+			.next()
+			.map(|rdf_types::Quad(s, p, o, g)| o.as_loc_quad(s, p, g))
+	}
+}
+
+pub struct IntoLocQuads<D: crate::SizedDataset>(D::IntoQuads)
+where
+	D::Graph: crate::SizedGraph;
+
+impl<O, F, D: crate::SizedDataset<Object = Object<O, F>>> Iterator for IntoLocQuads<D>
+where
+	D::Graph: crate::SizedGraph,
+{
+	type Item = LocQuad<D::Subject, D::Predicate, O, D::GraphLabel, F>;
+
+	fn next(&mut self) -> Option<Self::Item> {
+		self.0
+			.next()
+			.map(|rdf_types::Quad(s, p, o, g)| o.into_loc_quad(s, p, g))
 	}
 }
